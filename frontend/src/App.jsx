9c69632235be813563d08e48ff6f16e80e4147d7
akhila607 <vsrinu536@gmail.com>
2026-03-13 19:03:21 +0530

    Resolved merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,34 @@
 import { Routes, Route } from "react-router-dom";
+
+import LandingPage from "./components/Landingpage";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Upload from "./components/Upload";
 import ChatHistory from "./components/ChatHistory";
 import Account from "./components/Account";
-import "./styles/dashboard.css"; 
+import LearnMore from "./components/Learnmore";   // ADD THIS
+
+import "./styles/dashboard.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} /> 
-      <Route path="/login" element={<Login />} /> 
-      <Route path="/register" element={<Register />} /> 
+
+      {/* Landing Page */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* Learn More Page */}
+      <Route path="/learn-more" element={<LearnMore />} />
+
+      {/* Authentication */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Dashboard Pages */}
       <Route path="/upload" element={<Upload />} />
       <Route path="/chat-history" element={<ChatHistory />} />
       <Route path="/account" element={<Account />} />
+
     </Routes>
   );
 }
